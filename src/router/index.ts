b8b33839ec2d25d04e750d2493715aa287573f43
陈盛权 <1179293420@qq.com>
2021-06-17 16:44:33 +0800

    fix: 修改readme

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,40 +17,6 @@ const routes: Array<RouteRecordRaw> = [
         }
       }
     ]
-  },
-  {
-    path: '/signup',
-    name: 'Signup',
-    component: () => import('../views/login/signup.vue'),
-    meta: {
-      title: '注册'
-    }
-  },
-  {
-    path: '/verify',
-    name: 'Verify',
-    component: () => import('../views/login/verify.vue'),
-    meta: {
-      title: '验证码'
-    }
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: () => import('../views/user/index.vue'),
-    meta: {
-      title: '首页',
-      showTab: false,
-      keepAlive: true
-    }
-  },
-  {
-    path: '/editor',
-    name: 'Editor',
-    component: () => import('../views/editor/index.vue'),
-    meta: {
-      title: '富文本编辑器'
-    }
   }
 ]
 
